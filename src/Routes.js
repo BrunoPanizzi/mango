a08ce503960d1cb247dd98a42243c55a9454b7c5
Bruno Panizzi <brunopanizziq@gmail.com>
2021-12-24 17:17:29 -0300

    added navigation

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import Home from './screens/Home'
+import Login from './screens/Login'
+
+const Tab = createBottomTabNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Login' component={Login} />
+        <Tab.Screen name='Home' component={Home} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
